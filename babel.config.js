3dfafcb155e7cb4205139ae62c9b9d1af25762a8
Shashank <shashankpradhan@honeycomb.io>
2026-03-14 14:35:53 -0400

    Fix: inline EXPO_ROUTER_APP_ROOT via babel transform

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
+// Must be set before babel runs so transform-inline-environment-variables can substitute it
+process.env.EXPO_ROUTER_APP_ROOT = path.join(__dirname, 'app');
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      ['transform-inline-environment-variables', {
+        include: ['EXPO_ROUTER_APP_ROOT', 'EXPO_ROUTER_IMPORT_MODE'],
+      }],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
