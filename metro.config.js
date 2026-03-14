ac9133a4758189a55a7e152a93e1f6e69ef2bdee
Shashank <shashankpradhan@honeycomb.io>
2026-03-14 14:32:49 -0400

    Fix: set EXPO_ROUTER_APP_ROOT explicitly in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+// Set the app root for expo-router before metro processes files
+process.env.EXPO_ROUTER_APP_ROOT = path.join(__dirname, 'app');
 
 const config = getDefaultConfig(__dirname);
 
